02c7a7329b541bf0fd1e0c636db1a3c2ed8a1d76
cbintz <cbintz@mystrength.com>
2018-06-05 15:28:50 -0600

    wrap up droid (added recycler view)

--- Droid/MainActivity.cs
+++ Droid/MainActivity.cs
@@ -1,8 +1,13 @@
-﻿using Android.App;
+﻿using CorinneTest;
+using System;
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
 using Android.Widget;
 using Android.OS;
-using CorinneTest;
-using System;
+using Android.Support.V7.Widget;
+using System.Collections.Generic;
 
 namespace CorinneTest.Droid
 {
@@ -10,22 +15,14 @@ namespace CorinneTest.Droid
     [Activity(Label = "CorinneTest", MainLauncher = true, Icon = "@mipmap/icon")]
     public class MainActivity : Activity
     {
-        static Random rnd = new Random();
         #region Variables
-        TextView PersonName;
-        TextView PersonAge;
-        TextView PersonGender;
-        TextView PersonIndex;
-        TextView DaysAlive;
-        TextView MonthsAlive;
-        TextView AgeType;
-        TextView ZodiacSign;
-        TextView BirthDatePretty;
-        //bool isWoman;
+        RecyclerView mRecyclerView;
+        RecyclerView.LayoutManager mLayoutManager;
+        PersonListAdapter mAdapter;
+        static Random rnd = new Random();
         Button ChangePersonButton;
         Button NewListButton;
 
-        int Index = 0;
         #endregion
 
         #region Life Cycle
@@ -50,72 +47,43 @@ namespace CorinneTest.Droid
         {
             base.OnCreate(savedInstanceState);
             SetContentView(Resource.Layout.Main);
-            PersonDataSource.Instance.SetList(GenLowAge(), GenHighAge(),  GenNumPeople());
-            this.PersonName = FindViewById<TextView>(Resource.Id.name);
-            this.PersonAge = FindViewById<TextView>(Resource.Id.age);
-            this.PersonGender = FindViewById<TextView>(Resource.Id.gender);
-            this.PersonIndex = FindViewById<TextView>(Resource.Id.index);
-            this.DaysAlive = FindViewById<TextView>(Resource.Id.daysalive);
-            this.MonthsAlive = FindViewById<TextView>(Resource.Id.monthsalive);
-            this.AgeType = FindViewById<TextView>(Resource.Id.agetype);
-            this.ZodiacSign = FindViewById<TextView>(Resource.Id.zodiac);
-            this.BirthDatePretty = FindViewById<TextView>(Resource.Id.birthdatepretty);
+            PersonDataSource.Instance.SetList(GenLowAge(), GenHighAge(), GenNumPeople());
+
             this.ChangePersonButton = FindViewById<Button>(Resource.Id.change_button);
             this.ChangePersonButton.Click += ChangePersonButton_Click;
+
             this.NewListButton = FindViewById<Button>(Resource.Id.newlist_button);
             this.NewListButton.Click += NewListButton_Click;
-            loadPerson(this.Index);
 
+            // Get RecyclerView layout:
+            mRecyclerView = FindViewById<RecyclerView>(Resource.Id.recyclerView);
 
+            // Plug in the linear layout manager:
+            mLayoutManager = new LinearLayoutManager(this);
+            mRecyclerView.SetLayoutManager(mLayoutManager);
 
+            loadRecycler();
+        }
+
+        void loadRecycler() {
+            // Plug in my adapter:
+            mAdapter = new PersonListAdapter();
+            mRecyclerView.SetAdapter(mAdapter);
         }
         #endregion
 
         #region Callback
         void ChangePersonButton_Click(object sender, System.EventArgs e)
         {
-            if (this.Index < PersonDataSource.Instance.People.Count) { loadPerson(this.Index + 1); }
-            else { loadPerson(this.Index); }
-            
+            // nothing here, let's change this to do something else down the line (or not sinc we ahve to move to iOS)
         }
 
         void NewListButton_Click(object sender, System.EventArgs e)
         {
             PersonDataSource.Instance.SetList(GenLowAge(), GenHighAge(), GenNumPeople());
-            this.Index = 0;
-            loadPerson(this.Index);
-
+            loadRecycler();
         }
 
-
-        void loadPerson(int index) {
-            int birthDateType = rnd.Next(0, 2);
-            this.Index = index;
-            var person = PersonDataSource.Instance.GetPerson(index);
-            //if (person.Gender == Gender.Woman) { isWoman = true; }
-            this.PersonName.Text = person.Name;
-            this.PersonAge.Text = person.Age.ToString();
-            this.PersonGender.Text = person.Gender.ToString();
-            this.PersonIndex.Text = "Index: " + this.Index.ToString();
-            this.DaysAlive.Text = "Days Alive: " + person.BirthDate.DaysAlive().ToString();
-            this.MonthsAlive.Text = "Months Alive: " + person.BirthDate.MonthsAlive().ToString();
-            this.AgeType.Text = "Age Type: " + person.BirthDate.AgeType();
-            this.ZodiacSign.Text = "Zodiac Sign: " + person.BirthDate.ZodaicSign();
-            this.BirthDatePretty.Text = "Birthdate: "  + person.BirthDate.PrintPretty();
-            switch (birthDateType)
-            {
-                case 0: 
-                    this.BirthDatePretty.Text = "Birthdate: " + person.BirthDate.PrintPretty();
-                    break;
-                case 1:
-                    this.BirthDatePretty.Text = "Birthdate: " + person.BirthDate.PrintStandardUS();
-                    break;
-                default:
-                    this.BirthDatePretty.Text = "Birthdate: " + person.BirthDate.PrintStandardNotUS();
-                    break;
-
-            }
-        }
         #endregion
     }
 }
